45e2da76d50a5c0cb3667e10f275e62bde5845bc
Muhammad Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2021-04-29 09:57:21 +0700

    uts

---
+++ uts/4.py
@@ -0,0 +1,24 @@
+def bubbleSort(A):
+    n = len(A)
+    # A  = LIst itu sendiri
+    # n = panjang list
+
+    swap = False
+    # variable untuk menyimpan data list sementara
+    # variable untuk menampung data hasil yang ditukar
+
+    # algorithm
+    while not swap:
+        # keadaan true artinya harus di cek dengan loopingan
+        swap = True
+        for j in range(1, n):  # loop sebanyak 4 kali
+            if A[j-1] > A[j]:  # membandingkan 0 > 1
+                swap = False
+                temp = A[j]
+                A[j] = A[j-1]
+                A[j-1] = temp
+
+    return A
+
+
+print(bubbleSort([33, 11, 22, 99, 100, 102, 200, 50, 20]))
---
+++ uts/5.py
@@ -0,0 +1,36 @@
+def insertionSort(n, x):
+    temp = 0
+    for i in range(1, n):
+        temp = x[i]
+        j = i-1
+        while j >= 0 and temp < x[j]:
+            x[j+1] = x[j]
+            j = j-1
+            i = i-2
+        x[j+1] = temp
+    return x
+
+
+def binarysearch(k, n, A):
+    found = False
+    BB = 0
+    BA = n-1
+    while BB <= BA and not found:
+        mid = int((BA+BB)/2)
+        if A[mid] == k:
+            found = True
+            print("ketemu di index ke- ", mid, "and exist")
+        else:
+            if A[mid] > k:
+                BA = mid-1
+            else:
+                BB = mid+1
+    return found
+
+
+a2 = [145, 77, 20, 1, 120, 3, 4]
+print(a2, "Sebelum di sort ")
+a = (insertionSort(7, a2))
+print(a, "Sesudah di sort ")
+cari = int(input("Cari angka berapa : "))
+print(binarysearch(cari, 7, a))
